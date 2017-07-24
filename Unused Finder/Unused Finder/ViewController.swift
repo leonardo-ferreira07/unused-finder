a5d058ab7cdc31df77753171fac7e7c721736452
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-07-24 20:21:02 +0800

    Created project

---
+++ Unused Finder/Unused Finder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Unused Finder
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 24/07/17.
+//  Copyright © 2017 iOS Wizards. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
